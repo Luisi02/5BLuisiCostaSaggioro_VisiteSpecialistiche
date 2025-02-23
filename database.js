fc0aa5154f9f34b70275602213b58469ae14e41f
Luisi02 <157484161+Luisi02@users.noreply.github.com>
2025-02-23 15:37:37 +0000

    delete,select e conf

--- database.js
+++ database.js
@@ -22,20 +22,19 @@ const executeQuery = (sql) => {
 const database = {
    createTable: async () => {
       await executeQuery(`
-         CREATE TABLE IF NOT EXISTS accident (
+         CREATE TABLE IF NOT EXISTS type (
          id INT PRIMARY KEY AUTO_INCREMENT,
-         address varchar(255) NOT NULL,
-         date DATE NOT NULL,
-         time TIME NOT NULL,
-         injured int NOT NULL,
-         dead int NOT NULL)             
+         name varchar(20)
+         )             
       `);
       return await executeQuery(`
-         CREATE TABLE IF NOT EXISTS plates (
-            id INT PRIMARY KEY AUTO_INCREMENT,
-            plate VARCHAR(20) NOT NULL,
-            idAccident INT NOT NULL,
-            FOREIGN KEY (idAccident) REFERENCES accident(id) ON DELETE CASCADE)      
+         CREATE TABLE IF NOT EXISTS booking (
+         id int PRIMARY KEY AUTO_INCREMENT,
+         idType int NOT NULL,
+         date DATE NOT NULL,
+         hour INT NOT NULL,
+         name VARCHAR(50),
+         FOREIGN KEY (idType) REFERENCES type(id)      
       `);
    },
    insert: async (accident) => {
@@ -59,20 +58,16 @@ const database = {
          await executeQuery(sql);
       });
    },
-   delete: (id) => {
-      let sql = `
-        DELETE FROM accident
-        WHERE id=${id}
-           `;
-      return executeQuery(sql);
-   },
    select: async () => {
       let sql = `
-        SELECT id, address, date, time, injured, dead FROM accident 
+        SELECT name FROM type 
            `;
       const result = await executeQuery(sql);
       await Promise.all(result.map(async (accident) => {
-         sql = `
+         sql = ` 
+            SELECT b.id, t.name type, b.date, b.hour, b.name
+            FROM booking AS b
+            JOIN type as t ON b.idType = t.id
             SELECT plate FROM plates WHERE idAccident=${accident.id} 
            `;
          const list = await executeQuery(sql);
