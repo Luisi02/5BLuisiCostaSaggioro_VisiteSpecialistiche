4a1dac8f812e1009ed39862ca5010a1d290787c6
saggioroFederico <157488051+saggioz@users.noreply.github.com>
2025-02-25 10:41:34 +0000

    server

--- server.js
+++ server.js
@@ -2,24 +2,39 @@ const express = require("express");
 const http = require('http');
 const path = require('path');
 const app = express();
+const bodyParser = require('body-parser');
 const database = require("./database");
-database.createTable();
+database.createTables();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use("/", express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "node_modules/bootstrap/dist/"))); //permette accesso a bootstrap all'applicazione lato client
 
 app.post("/insert", async (req, res) => {
-  const accident = req.body.accident;
+  const booking = req.body;
   try {
-    await database.insert(accident);
+    await database.insert(booking);
     res.json({result: "ok"});
   } catch (e) {
+    console.error(e)
     res.status(500).json({result: "ko"});
   }
 })
-
-app.get('/booking', async (req, res) => {
-    const list = await database.select();
+app.get('/bookings', async (req, res) => {
+    const list = await database.selectBookings();
     res.json(list);
+});
+app.delete('/delete/:id', async (req, res) => {
+  await database.delete(req.params.id);
+  res.json({result: "ok"});
+})
+
+app.get('/types', async (req, res) => {
+  const list = await database.selectTypes();
+  res.json(list);
 });
 
 const server = http.createServer(app);
