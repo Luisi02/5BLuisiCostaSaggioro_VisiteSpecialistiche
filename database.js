c15898db0b741e70170d2325238f7bb7456603fc
saggioroFederico <157488051+saggioz@users.noreply.github.com>
2025-02-25 10:14:33 +0000

    database

--- database.js
+++ database.js
@@ -4,7 +4,7 @@ const conf = JSON.parse(fs.readFileSync('./public/conf.json'));
 conf.ssl = {
    ca: fs.readFileSync(__dirname + '/ca.pem')
 }
-const connection = mysql.createConnection(conf);
+const connection = mysql.createConnection(conf.db);
 
 const executeQuery = (sql) => {
    return new Promise((resolve, reject) => {
@@ -20,58 +20,46 @@ const executeQuery = (sql) => {
 }
 
 const database = {
-   createTable: async () => {
+   createTables: async () => {
       await executeQuery(`
-         CREATE TABLE IF NOT EXISTS type (
-         id INT PRIMARY KEY AUTO_INCREMENT,
-         name varchar(20)
-         )             
+        CREATE TABLE IF NOT EXISTS type (
+        id INT PRIMARY KEY AUTO_INCREMENT,
+        name varchar(20)
+        )
       `);
       return await executeQuery(`
-         CREATE TABLE IF NOT EXISTS booking (
-         id int PRIMARY KEY AUTO_INCREMENT,
-         idType int NOT NULL,
-         date DATE NOT NULL,
-         hour INT NOT NULL,
-         name VARCHAR(50),
-         FOREIGN KEY (idType) REFERENCES type(id)
-         )  
-      `);
+        CREATE TABLE IF NOT EXISTS booking (
+        id int PRIMARY KEY AUTO_INCREMENT,
+        idType int NOT NULL,
+        date DATE NOT NULL,
+        hour INT NOT NULL,
+        name VARCHAR(50),
+        FOREIGN KEY (idType) REFERENCES type(id));`);
    },
    insert: async (booking) => {
       let sql = `
-         INSERT INTO booking(date, hour, name)
-         VALUES (
-            '${booking.date}', 
-            '${booking.hour}', 
-            ${booking.name}
-           `;
+         INSERT INTO booking(idType, date, hour, name)
+         VALUES (${booking.idType},STR_TO_DATE('${booking.date}', '%Y-%m-%d'),${booking.hour},'${booking.name}');`;
       const result = await executeQuery(sql);
-      booking.type.forEach(async (element) => {
-         sql = `
-            INSERT INTO type(id, name) 
-            VALUES (
-               '${element}', 
-               ${result.insertId})
-         `;
-         await executeQuery(sql);
-      });
    },
-   select: async () => {
+   delete: (id) => {
       let sql = `
-        SELECT name FROM type 
+        DELETE FROM booking
+        WHERE id=${id}
+           `;
+      return executeQuery(sql);
+   },
+   selectBookings: async () => {
+      let sql = `
+        SELECT b.id, t.name type, b.date, b.hour, b.name
+        FROM booking AS b
+        JOIN type as t ON b.idType = t.id
            `;
       const result = await executeQuery(sql);
-      await Promise.all(result.map(async (booking) => {
-         sql = ` 
-            SELECT b.id, t.name type, b.date, b.hour, b.name
-            FROM booking AS b
-            JOIN type as t ON b.idType = t.id
-            WHERE date='aaaa-mm-gg'
-           `;
-         const list = await executeQuery(sql);
-         booking.type = list.map(p => p.plate);
-      }));
+      return result;
+   },
+   selectTypes: async () => {
+      const result = await executeQuery("SELECT name FROM type");
       return result;
    },
    drop: async () => {
