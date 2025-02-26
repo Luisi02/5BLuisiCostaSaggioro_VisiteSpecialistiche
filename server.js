619b4f8d24b4e98ebdf05f04f6745e6774cd2677
saggioroFederico <157488051+saggioz@users.noreply.github.com>
2025-02-26 11:42:30 +0000

    rifacimento, non mi convinceva

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 app.use("/", express.static(path.join(__dirname, "public")));
-app.use(express.static(path.join(__dirname, "node_modules/bootstrap/dist/"))); //permette accesso a bootstrap all'applicazione lato client
+app.use(express.static(path.join(__dirname, "node_modules/bootstrap/dist/")));
 
 app.post("/insert", async (req, res) => {
   const booking = req.body;
