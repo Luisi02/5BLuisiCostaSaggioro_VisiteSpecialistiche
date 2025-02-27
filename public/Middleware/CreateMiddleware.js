1c630af035c2a58f0a58febca9490b891b5c998b
saggioroFederico <157488051+saggioz@users.noreply.github.com>
2025-02-27 23:13:30 +0000

    middleware

--- public/Middleware/CreateMiddleware.js
+++ public/Middleware/CreateMiddleware.js
@@ -1,23 +1,59 @@
 export const createMiddleware = () => {
     return {
         load: async () => {
-            const response = await fetch("/booking");
-            return await response.json();
+            const response = await fetch("http://localhost:5600/bookings");
+            const json = await response.json();
+            return json;
+        },
+        loadByDate: async (date) => {
+            const response = await fetch("http://localhost:5600/bookings/date/" + date);
+            const json = await response.json();
+            return json;
         },
         delete: async (id) => {
-            const response = await fetch("/delete/" + id, {
-                method: 'DELETE',
+            const response = await fetch("http://localhost:5600/delete-booking/" + id, {
+                method: "DELETE",
+            });
+            const json = await response.json();
+            return json;
+        },
+        add: async (booking) => {
+            const response = await fetch("http://localhost:5600/insert-booking", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({ booking: booking }),
+            });
+            const json = await response.json();
+            return json;
+        },
+        truncateTables: async () => {
+            const response = await fetch("http://localhost:5600/truncate-tables", {
+                method: "DELETE",
+            });
+            const json = await response.json();
+            return json;
+        },
+        dropTables: async () => {
+            const response = await fetch("http://localhost:5600/drop-tables", {
+                method: "DELETE",
+            });
+            const json = await response.json();
+            return json;
+        },
+        addType: async (nametype) => {
+            const response = await fetch("http://localhost:5600/insert-type", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({ name: nametype }),
             });
             return await response.json();
         },
-        add: async (booking) => {
-            const response = await fetch("/insert", {
-                method: 'POST',
-                headers: {
-                    "content-type": "application/json"
-                },
-                body: JSON.stringify({ booking: booking })
-            });
+        loadTypes: async () => {
+            const response = await fetch("http://localhost:5600/types");
             return await response.json();
         }
     };
