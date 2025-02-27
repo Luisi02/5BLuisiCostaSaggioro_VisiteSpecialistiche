844095dcdba5e8199db4d3e6539b074af369f19b
Luisi02 <157484161+Luisi02@users.noreply.github.com>
2025-02-27 16:43:33 +0000

    Login

---
+++ public/Login/CreateLogin.js
@@ -0,0 +1,33 @@
+//Componente LOGIN
+export const createLogin = () => {
+    let isLogged = false;
+    return {
+        checkLogin: (username, password) => {
+            return new Promise ((resolve,reject) => {
+                fetch("conf.json").then((response) => response.json()).then((confData) => {
+                    fetch("https://ws.cipiaceinfo.it/credential/login", {
+                        method: "POST",
+                        headers: {
+                        "Content-Type": "application/json",
+                        key: confData.loginToken,
+                        },
+                        body: JSON.stringify({ username, password }),
+                    })
+                    .then((response) => response.json())
+                    .then((result) => {
+                        resolve(result.result);
+                    })
+                    .catch((error) => {
+                        console.error("Errore login:", error);
+                        alert("Errore");
+                        reject (result);
+                    });
+                })
+            });
+        },
+        validateLogin: () => {
+            isLogged = true;
+        },
+    }
+  return
+};
